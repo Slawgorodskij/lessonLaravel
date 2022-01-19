ba98ffd1ac317a3eb860df7d80fe924aef999e00
Slawgorodskij <Slawgorodskij@mail.ru>
2022-01-19 16:57:26 +0700

    preparation

--- app/Http/Controllers/CategoryNewsController.php
+++ app/Http/Controllers/CategoryNewsController.php
@@ -7,34 +7,12 @@ use Illuminate\Http\Request;
 class CategoryNewsController extends Controller
 {
     private string $title = 'Категории новостей';
-    private array $listCategory = [
-        [
-            'nameCategory' => 'авто',
-            'news' => [
-                '1' => 'Когда появится Tesla Cybertruck? Сроки опять сдвигаются',
-                '2' => 'Hyundai везет в Россию сразу 5 новинок в 2022 году',
-                '3' => 'Toyota показала бешеную малолитражку GRMN Yaris.',
-                '4' => 'Эка логика',
-            ],
-        ],
-        [
-            'nameCategory' => 'здоровье',
-            'news' => [
-                '1' => 'Названа привлекательная сторона ношения масок при коронавирусе',
-                '2' => 'ВОЗ рекомендовала два новых препарата для лечения пациентов с COVID-19',
-                '3' => 'Названа привлекательная сторона ношения масок при коронавирусе',
-                '4' => 'Ягоды годжи защищают от возрастной потери зрения',
-            ],
-        ],
-
-    ];
-
 
     public function category()
     {
         return view('category', [
             'title' => $this->title,
-            'listCategory' => $this->listCategory,
+            'listCategory' => '',
         ]);
     }
 }
--- resources/views/templates/introduction.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="introduction">
-    <div class="introduction__item container">
-        <h1 class="introduction__title">{{$title}}</h1>
-    </div>
-</div>
